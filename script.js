2600c6f9cc14d59cdbea64a7645172d27d31644a
Far9999 <sadikalhanssah@gmail.com>
2023-12-31 22:46:34 +0000

    add more ui and logic

--- script.js
+++ script.js
@@ -2,46 +2,68 @@ const container = document.querySelector('.container');
 const height = container.offsetHeight;
 const width = container.offsetWidth;
 
+const select = document.querySelector('#select');
+const eraser = document.querySelector('#eraser');
+const special = document.querySelector('#special');
+const defaultColor = document.querySelector('#default');
+
 function gridGenerator(squares){
     const area = squares**2;
 
     for (let i=0; i < area; i++){
         const div = document.createElement('div');
         div.className = "grid";
-        div.style.width = `${(width-10)/squares}px`;
-        div.style.height = `${(height)/squares}px`;
+        div.style.width = `${(width-6)/squares}px`;
+        div.style.height = `${(height-6)/squares}px`;
         container.appendChild(div);
     };
 };
+
+function randomColor(){
+    return `rgb(${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)})`;
+}
 
 function colorGrid(color){
     const grid = document.querySelectorAll('.grid');
 
     grid.forEach((item)=>{
         item.addEventListener('dragover', function (){
-            item.style.backgroundColor = color;
-            console.log(item.style.backgroundColor);
+            if (color!=='rainbow'){
+                item.style.backgroundColor = color;
+            } else {
+                item.style.backgroundColor = randomColor();
+            }
         });
     });
 };
 
-gridGenerator(10);
-colorGrid('black');
-
-const select = document.querySelector('#select');
+window.onload = ()=>{
+    gridGenerator(10);
+    colorGrid('black');
+};
 
 select.addEventListener('click', ()=>{
-    alert('Enter number of squares. Please DO NOT enter a number more than 100 ');
-    squares = parseInt(window.prompt("Enter number: ", 0));
+    squares = parseInt(window.prompt("Enter a number not more than 100: ", 0));
     
+    container.innerHTML = "";
     gridGenerator(squares);
     colorGrid('black');
 });
 
-const eraser = document.querySelector('#eraser');
-
 eraser.addEventListener('click', ()=>{
     colorGrid('white');
+});
+
+special.addEventListener('click', ()=>{
+    colorGrid('rainbow');
+});
+
+special.addEventListener('click', ()=>{
+    colorGrid('rainbow');
+});
+
+defaultColor.addEventListener('click', ()=>{
+    colorGrid('black');
 });
 
 
